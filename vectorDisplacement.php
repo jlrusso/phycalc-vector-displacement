894487181d0f5b97d04cfc678bc6b068743b8d8b
James Russo <1jlrusso@gmail.com>
2018-04-04 16:10:40 -0700

    replaced 'practice-btns-outer' with link to external file that contains it using php 'include' statement

--- vectorDisplacement.php
+++ vectorDisplacement.php
@@ -241,13 +241,9 @@
 
 			<div class="line-divider"></div>
 
-			<div class="row">
-				<div class="text-center" id="practice-btns-container">
-					<button type="button" class="practice-btn" data-toggle="modal" data-target="#my-modal-one">Example 1</button>
-					<button type="button" class="practice-btn" data-toggle="modal" data-target="#my-modal-two">Example 2</button>
-					<button type="button" class="practice-btn" data-toggle="modal" data-target="#my-modal-three">Example 3</button>
-				</div>
-			</div>
+			<?php 
+				include_once "practice.php";
+			?>
 
 
 			<div class="modal fade example-modal" id="my-modal-one" role="dialog">
